30d2570510c981c115b40903936cfd973e196a0e
RizkyNurMuzhaki22 <rizki.nurmuzhaki@gmail.com>
2023-07-31 15:04:51 +0700

    Belajar Routes

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>Home</h1>
+    </div>
+</template>
+
+<script>
+import { onMounted } from 'vue';
+
+export default {
+    setup(props, context)
+{
+    onMounted(() => {
+    context.emit("id-menu", 1);
+})
+}
+}
+
+</script>
